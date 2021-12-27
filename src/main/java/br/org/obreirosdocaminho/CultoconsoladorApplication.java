5e64f5402f0a2142beda18436e404dc782936063
Tulio Henrique Alves dos Santos <tuliopsy@gmail.com>
2021-12-27 14:52:41 -0300

    Commit inicial

---
+++ src/main/java/br/org/obreirosdocaminho/CultoconsoladorApplication.java
@@ -0,0 +1,13 @@
+package br.org.obreirosdocaminho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultoconsoladorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultoconsoladorApplication.class, args);
+	}
+
+}
